c451c3c8a55875b5aca14806fdfcb8d12fa37324
Gustavosouza2 <gustavoleonsouza@gmail.com>
2023-06-21 15:22:08 -0300

    feat: finalização do component projetos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,17 @@ import Navbar from "../components/Navbar";
 import { Homepage } from "@/components/Homepage";
 import { About } from "@/components/About";
 import { Projects } from "@/components/Projects";
+import { Contact } from "@/components/Contact";
 
 export default function Home() {
   return (
     <>
-      {" "}
+      {" Importações dos Components "}
       <Navbar />
       <Homepage />
       <About />
       <Projects />
+      <Contact />
     </>
   );
 }
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,195 +1,235 @@
 import React from "react";
 import QuartoBranco from "../projetos/quartobranco.jpg";
-
+import Cozinha from "../projetos/cozinha.jpg";
+import salapreta from "../projetos/salapreta.jpg";
+import cozinhapreta from "../projetos/cozinhapreta.jpg";
+import lustre from "../projetos/lustrebranco.jpg";
+import cozinhabranca from "../projetos/cozinhabranca.jpg";
+import cozinhamoderna from "../projetos/cozinhamoderna.jpg";
+import Sala from "../projetos/sala.jpg";
+import quartopreto from "../projetos/quartopreto.jpg";
+import banheiro from "../projetos/banheiropreto.jpg";
 
 export const Projects = () => {
   return (
-    <div className="w-full md:h-screen bg-[#f6f6f6] dark:bg-slate-100 text-gray-300">
+    <div className="w-full h-full md:h-full bg-[#f6f6f6] dark:bg-slate-100 text-gray-300">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
-        <div className="pb-8 flex justify-center items-center">
+        <div className="pb-8 flex justify-center items-center mt-24 md:mt-0">
           <p className="text-2xl text-silver font-sans  tracking-medium underline dark:text-[#111112]">
             Arquitetura
           </p>
         </div>
 
         {/* Container */}
-        <div className="grid sm:grid-cols-2 md:grid-cols-2 gap-10">
+        <div className="grid sm:grid-cols-2 md:grid-cols-2 gap-40 mt-14">
           {/* Grid */}
           <div
-          style={{ backgroundImage: `url(${QuartoBranco.src})`,
-          width: '100%',
-          height: '100%', }}
-          className="shadow-lg shadow-[#040C16] group container rounded-md flex justify-center items-center mx-auto content-div">
+            style={{
+              backgroundImage: `url(${QuartoBranco.src})`,
+              width: "90%",
+              height: "160%",
+            }}
+            className="shadow-lg bg-cover bg-center shadow-[#040C16] group container flex justify-start items-end mx-auto content-div"
+          >
             {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100 transition delay-100 duration-300 ease-in-out">
-              <span className="text 2xl font-bold text-white tracking-wider">
-                myCoffe
-              </span>
+            <div className="opacity-0 group-hover:opacity-100 transition delay-100 duration-300 ease-in-out pt-32 w-full">
+              <div className="hover:bg-[#f6f6f6a8] flex flex-col px-4 transition delay-100 duration-300 ease-in-out pb-2 pt-2">
+                <span className="text-2xl font-sans text-silver tracking-widest">
+                  Quarto Moderno
+                </span>
 
-              <div className="pt-8 text-center">
-                <a
-                  href="https://mycoffeesite.netlify.app/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href="https://github.com/Gustavosouza2/myCoffe"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
+                <div className="pt-1 text-justify text-1xl font-sans text-silver">
+                  <p>São Paulo, SP</p>
+                </div>
               </div>
             </div>
           </div>
-          <div className="shadow-lg shadow-[#040C16] group container rounded-md flex justify-center items-center mx-auto content-div">
+          <div
+            style={{
+              backgroundImage: `url(${Cozinha.src})`,
+              width: "90%",
+              height: "160%",
+            }}
+            className="shadow-lg bg-cover bg-center shadow-[#040C16] group container flex justify-start items-end mx-auto content-div"
+          >
             {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100 transition delay-100 duration-300 ease-in-out">
-              <span className="text 2xl font-bold text-white tracking-wider">
-                Design System
-              </span>
+            <div className="opacity-0 group-hover:opacity-100 transition delay-100 duration-300 ease-in-out pt-32 w-full">
+              <div className="hover:bg-[#f6f6f6a8] flex flex-col px-4 transition delay-100 duration-300 ease-in-out pb-2 pt-2">
+                <span className="text-2xl font-sans text-silver tracking-widest">
+                  Cozinha
+                </span>
 
-              <div className="pt-8 text-center">
-                <a
-                  href="https://gustavosouza2.github.io/lab-system/?path=/story/components-button--default"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href="https://github.com/Gustavosouza2/lab-system"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
+                <div className="pt-1 text-justify text-1xl font-sans text-silver">
+                  <p>São Paulo, SP</p>
+                </div>
               </div>
             </div>
           </div>
-          <div className="shadow-lg shadow-[#040C16] group container rounded-md flex justify-center items-center mx-auto content-div">
+          <div
+            style={{
+              backgroundImage: `url(${salapreta.src})`,
+              width: "90%",
+              height: "160%",
+            }}
+            className="shadow-lg bg-cover bg-center shadow-[#040C16] group container flex justify-start items-end mx-auto content-div"
+          >
             {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100 transition delay-100 duration-300 ease-in-out">
-              <span className="text 2xl font-bold text-white tracking-wider">
-                Login
-              </span>
+            <div className="opacity-0 group-hover:opacity-100 transition delay-100 duration-300 ease-in-out pt-32 w-full">
+              <div className="hover:bg-[#f6f6f6a8] flex flex-col px-4 transition delay-100 duration-300 ease-in-out pb-2 pt-2">
+                <span className="text-2xl font-sans text-silver tracking-widest">
+                  Sala Preta
+                </span>
 
-              <div className="pt-8 text-center">
-                <a
-                  href="https://react-login-brown.vercel.app/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href="https://github.com/Gustavosouza2/React-Login"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
+                <div className="pt-1 text-justify text-1xl font-sans text-silver">
+                  <p>São Paulo, SP</p>
+                </div>
               </div>
             </div>
           </div>
-          <div className="shadow-lg shadow-[#040C16] group container rounded-md flex justify-center items-center mx-auto content-div">
+          <div
+            style={{
+              backgroundImage: `url(${cozinhapreta.src})`,
+              width: "90%",
+              height: "160%",
+            }}
+            className="shadow-lg bg-cover bg-center shadow-[#040C16] group container flex justify-start items-end mx-auto content-div"
+          >
             {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100 transition delay-100 duration-300 ease-in-out">
-              <span className="text 2xl font-bold text-white tracking-wider">
-                Controle Financeiro
-              </span>
+            <div className="opacity-0 group-hover:opacity-100 transition delay-100 duration-300 ease-in-out pt-32 w-full">
+              <div className="hover:bg-[#f6f6f6a8] flex flex-col px-4 transition delay-100 duration-300 ease-in-out pb-2 pt-2">
+                <span className="text-2xl font-sans text-silver tracking-widest">
+                  Cozinha Preta
+                </span>
 
-              <div className="pt-8 text-center">
-                <a
-                  href="https://controle-financeiro-sable.vercel.app/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href="https://github.com/Gustavosouza2/Controle-Financeiro"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
+                <div className="pt-1 text-justify text-1xl font-sans text-silver">
+                  <p>São Paulo, SP</p>
+                </div>
               </div>
             </div>
           </div>
-          <div className="shadow-lg shadow-[#040C16] group container rounded-md flex justify-center items-center mx-auto content-div">
+          <div
+            style={{
+              backgroundImage: `url(${lustre.src})`,
+              width: "90%",
+              height: "160%",
+            }}
+            className="shadow-lg bg-cover bg-center shadow-[#040C16] group container flex justify-start items-end mx-auto content-div"
+          >
             {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100 transition delay-100 duration-300 ease-in-out">
-              <span className="text 2xl font-bold text-white tracking-wider">
-                Netflix
-              </span>
+            <div className="opacity-0 group-hover:opacity-100 transition delay-100 duration-300 ease-in-out pt-32 w-full">
+              <div className="hover:bg-[#f6f6f6a8] flex flex-col px-4 transition delay-100 duration-300 ease-in-out pb-2 pt-2">
+                <span className="text-2xl font-sans text-silver tracking-widest">
+                  Lustre
+                </span>
 
-              <div className="pt-8 text-center">
-                <a
-                  href="https://netflix-clone-one-tau.vercel.app/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href="https://github.com/Gustavosouza2/Netflix"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
+                <div className="pt-1 text-justify text-1xl font-sans text-silver">
+                  <p>São Paulo, SP</p>
+                </div>
               </div>
             </div>
           </div>
-          <div className="shadow-lg shadow-[#040C16] group container rounded-md flex justify-center items-center mx-auto content-div">
+          <div
+            style={{
+              backgroundImage: `url(${cozinhabranca.src})`,
+              width: "90%",
+              height: "160%",
+            }}
+            className="shadow-lg bg-cover bg-center shadow-[#040C16] group container flex justify-start items-end mx-auto content-div"
+          >
             {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100 transition delay-100 duration-300 ease-in-out">
-              <span className="text 2xl font-bold text-white tracking-wider">
-                RocketPay
-              </span>
+            <div className="opacity-0 group-hover:opacity-100 transition delay-100 duration-300 ease-in-out pt-32 w-full">
+              <div className="hover:bg-[#f6f6f6a8] flex flex-col px-4 transition delay-100 duration-300 ease-in-out pb-2 pt-2">
+                <span className="text-2xl font-sans text-silver tracking-widest">
+                  Cozinha branca
+                </span>
 
-              <div className="pt-8 text-center">
-                <a
-                  href="https://rocketpay1.netlify.app/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href="https://github.com/Gustavosouza2/rocketpay"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
+                <div className="pt-1 text-justify text-1xl font-sans text-silver">
+                  <p>São Paulo, SP</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{
+              backgroundImage: `url(${cozinhamoderna.src})`,
+              width: "90%",
+              height: "160%",
+            }}
+            className="shadow-lg bg-cover bg-center shadow-[#040C16] group container flex justify-start items-end mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100 transition delay-100 duration-300 ease-in-out pt-32 w-full">
+              <div className="hover:bg-[#f6f6f6a8] flex flex-col px-4 transition delay-100 duration-300 ease-in-out pb-2 pt-2">
+                <span className="text-2xl font-sans text-silver tracking-widest">
+                  Cozinha Moderna
+                </span>
+
+                <div className="pt-1 text-justify text-1xl font-sans text-silver">
+                  <p>São Paulo, SP</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{
+              backgroundImage: `url(${Sala.src})`,
+              width: "90%",
+              height: "160%",
+            }}
+            className="shadow-lg bg-cover bg-center shadow-[#040C16] group container flex justify-start items-end mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100 transition delay-100 duration-300 ease-in-out pt-32 w-full">
+              <div className="hover:bg-[#f6f6f6a8] flex flex-col px-4 transition delay-100 duration-300 ease-in-out pb-2 pt-2">
+                <span className="text-2xl font-sans text-silver tracking-widest">
+                  Sala de estar
+                </span>
+
+                <div className="pt-1 text-justify text-1xl font-sans text-silver">
+                  <p>São Paulo, SP</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{
+              backgroundImage: `url(${quartopreto.src})`,
+              width: "90%",
+              height: "160%",
+            }}
+            className="shadow-lg bg-cover bg-center shadow-[#040C16] group container flex justify-start items-end mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100 transition delay-100 duration-300 ease-in-out pt-32 w-full">
+              <div className="hover:bg-[#f6f6f6a8] flex flex-col px-4 transition delay-100 duration-300 ease-in-out pb-2 pt-2">
+                <span className="text-2xl font-sans text-silver tracking-widest">
+                  Quarto preto
+                </span>
+
+                <div className="pt-1 text-justify text-1xl font-sans text-silver">
+                  <p>São Paulo, SP</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{
+              backgroundImage: `url(${banheiro.src})`,
+              width: "90%",
+              height: "160%",
+            }}
+            className="shadow-lg bg-cover bg-center shadow-[#040C16] group container flex justify-start items-end mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100 transition delay-100 duration-300 ease-in-out pt-32 w-full">
+              <div className="hover:bg-[#f6f6f6a8] flex flex-col px-4 transition delay-100 duration-300 ease-in-out pb-2 pt-2">
+                <span className="text-2xl font-sans text-silver tracking-widest">
+                  Banheiro moderno
+                </span>
+
+                <div className="pt-1 text-justify text-1xl font-sans text-silver">
+                  <p>São Paulo, SP</p>
+                </div>
               </div>
             </div>
           </div>
