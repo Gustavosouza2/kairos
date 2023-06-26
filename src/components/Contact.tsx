d17a1dda23142c7bea8de0693c3505a206335350
Gustavosouza2 <gustavoleonsouza@gmail.com>
2023-06-26 17:22:51 -0300

    setimo commit

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import imgcontact from "../images/contact.jpg";
+import imgcontact from "../images/contatoimg.jpg";
 import logocontact from '../images/Logocontact.png';
 
 export const Contact = () => {
